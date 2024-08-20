122a47b2bf0ec87a7db0faf63b6507c30e35b771
Isaak <111081490+isaakgd0@users.noreply.github.com>
2024-08-19 21:09:30 -0300

    Update index.js
    Disposição dos itens na tela refeita. Bem mais prática agora, usando um array com as informações dos produtos e o método map para distribuí-los.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,31 +3,32 @@ import { Link } from 'react-router-dom';
 import '../Estilos/Header.css';
 import './Home.css';
 
+//IMPORTANDO IMAGENS DOS PRODUTOS:
 import geladeiraImage from './imgs/geladeira.jpg';
 import arCondicionadoImage from './imgs/arCondicionado.jpg';
 import lavadoraImage from './imgs/lavadora.jpg';
 import tvImage from './imgs/TV.jpg';
 import fogaoImage from './imgs/fogao.jpg';
 
+//ARRAY DOS PRODUTOS:
+const items = [
+  {id: 1, img: geladeiraImage, nome:"Geladeira Frost Free Duplex de Inox - 375L", precoAntigo: "5.999,99", preco: "2.999,99", info:"Geladeiras de altíssima qualidade em oferta!! Só hoje!!"},
+  {id: 2, img: arCondicionadoImage, nome: "Ar Condicionado 220 Volts", precoAntigo: "4.900,00", preco: "2.499,99", info: "Aparelhos de ar condicionado de altíssima qualidade em oferta!! Só hoje!!"},
+  {id: 3, img: lavadoraImage, nome: "Lavadora de Roupas Premium Care 11Kg", precoAntigo: "3.499,99", preco: "1.749.99", info: "Lavadoras e Secadoras de altíssima qualidade em oferta!! Só Hoje!!",},
+  {id: 4, img: tvImage, nome: "Tevelisor Samsung 32' SmarTV", precoAntigo: "4.249,99", preco: "2.124.99", info: "Tevelisores de atíssima qualidade em oferta!! Só hoje!!",},
+  {id: 5, img: fogaoImage, nome: "Fogão 5 Bocas com Acendimento Automático – Preto", precoAntigo: "2.199,99", precoAntigo: "1.99,99", info: "Fogões de altíssima qualidade em oferta!! Só hoje!!",},
+];
+
+//ARRAY DO CARRINHO:
+export const arrayCarrinho = [];
+
 function Home() {
   const [arrayCarrinho, setArrayCarrinho] = useState([]);
 
-  const addCarrinho = (nomeProduto, preco) => {
-    setArrayCarrinho([...arrayCarrinho, { nomeProduto, preco }]);
-  };
-
-  const calcularValorTotal = () => {
-    let total = 0;
-    arrayCarrinho.forEach(item => {
-      total += item.preco;
-    });
-    return total.toFixed(2);
-  };
-
-  const fecharPedido = () => {
-    setArrayCarrinho([]);
-    alert("Seu pedido foi fechado! Qualquer atualização, te mandaremos por E-Mail!");
-  };
+  const addCarrinho = (nome, preco) =>{
+    setArrayCarrinho([...arrayCarrinho, {nome, preco}]);
+    alert("Item adicionado!");
+  }
 
   return (
     <div>
@@ -42,127 +43,30 @@ function Home() {
           <Link to="/PagLogin">
             <button className='botaoCadastro'>Cadastre-se</button>
           </Link>
+
         </div>
       </div>
 
-      <div className='carrinhodeCompras'>
-        <div className='headCarrinho'>
-          <h3>Seu Carrinho</h3>
-        </div>
-        <div className='doCarrinho'>
-          {arrayCarrinho.length > 0 ? (
-            <ul>
-              {arrayCarrinho.map((item, index) => (
-                <li key={index}>{item.nomeProduto}</li>
-              ))}
-            </ul>
-          ) : (
-            <p>Carrinho vazio</p>
-          )}
-        </div>
-        <div className='totalCompra'>
-          <span>Total da compra: R$ {calcularValorTotal()}</span>
-          <br />
-          <button className='botaoFinal' onClick={fecharPedido}>Fechar Pedido</button>
-        </div>
-      </div>
-
-      <div className='bannerOfertas'>
-        <h3 className='tituloPiscante'>OFERTAS SÓ HOJE!!!</h3>
-        <span>Até 95% de desconto!</span>
-      </div>
-
-      <div className='divBonita'>
-        <div className='botaoProduto'>
-          <img src={geladeiraImage} title="Compra aí, mané" />
-          <div className='info'>
-            <span className='nomeProduto'>Geladeira Frost Free Duplex de Inox - 375L</span>
-            <br />
-            <br />
-            <span className='precoAntigo'>De R$ 5.999,99</span>
-            <br />
-            <span className='precoProduto'>Por R$ 4.899,99</span>
-            <br />
-            <span className='infoProduto'>Geladeiras de altíssima qualidade em oferta!! Só hoje!!.</span>
-            <br />
-            <button className='botaoCarrinho' onClick={() => addCarrinho("Geladeira Brastemp BRM44HK", 4899.99)}>Adicionar ao Carrinho</button>
+      {/* DISPOSIÇÃO DOS ITEMS NA TELA */}
+      {items.map(item => (
+        <div className='divBonita'>
+          <div className='botaoProduto'>
+            <img src={item.img} title='compra aí' />
+            <div className='info'>
+              <span className='nomeProduto'>{item.nome}</span>
+              <br />
+              <br />
+              <span className='precoAntigo'>De R${item.precoAntigo}</span>
+              <br />
+              <span className='precoProduto'>R${item.preco}</span>
+              <br />
+              <span className='infoProduto'>{item.info}</span>
+              <br />
+              <button className='botaoCarrinho' onClick={() => addCarrinho(item.nome, item.preco)}>Adicionar ao Carrinho</button>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div className='divBonita'>
-        <div className='botaoProduto'>
-          <img src={arCondicionadoImage} />
-          <div className='info'>
-            <span className='nomeProduto'>Ar Condicionado 220 Volts</span>
-            <br />
-            <br />
-            <br />
-            <span className='precoAntigo'>De R$ 4.899,99</span>
-            <br />
-            <span className='precoProduto'>Por R$ 3.989,90</span>
-            <br />
-            <span className='infoProduto'>Aparelhos de ar condicionado de altíssima qualidade em oferta!! Só hoje!!</span>
-            <br />
-            <button className='botaoCarrinho' onClick={() => addCarrinho("Ar Condicionado Split Hi Wall", 3989.9)}>Adicionar ao Carrinho</button>
-          </div>
-        </div>
-      </div>
-
-      <div className='divBonita'>
-        <div className='botaoProduto'>
-          <img src={lavadoraImage} />
-          <div className='info'>
-            <span className='nomeProduto'>Lavadora de Roupas Premium Care 11Kg </span>
-            <br />
-            <br />
-            <br />
-            <span className='precoAntigo'>De R$ 5.999,99</span>
-            <br />
-            <span className='precoProduto'>R$ 199,90</span>
-            <br />
-            <span className='infoProduto'>Lavadoras e Secadoras de altíssima qualidade em oferta!! Só Hoje!!</span>
-            <br />
-            <button className='botaoCarrinho' onClick={() => addCarrinho("Lavadora de Roupas Premium Care 11Kg", 199.9)}>Adicionar ao Carrinho</button>
-          </div>
-        </div>
-      </div>
-
-      <div className='divBonita'>
-        <div className='botaoProduto'>
-          <img src={tvImage} />
-          <div className='info'>
-            <span className='nomeProduto'>Tevelisor Samsung 32' SmarTV</span>
-            <br />
-            <span className='precoAntigo'>De R$ 7.999,99</span>
-            <br />
-            <span className='precoProduto'>R$ 3.999,90</span>
-            <br />
-            <span className='infoProduto'>Tevelisores de atíssima qualidade em oferta!! Só hoje!!</span>
-            <br />
-            <button className='botaoCarrinho' onClick={() => addCarrinho("Tevelisor Samsung 32' SmarTV", 3999.9)}>Adicionar ao Carrinho</button>
-          </div>
-        </div>
-      </div>
-
-      <div className='divBonita'>
-        <div className='botaoProduto'>
-          <img src={fogaoImage} />
-          <div className='info'>
-            <span className='nomeProduto'>Fogão 5 Bocas com Acendimento Automático – Preto</span>
-            <br />
-            <br />
-            <br />
-            <span className='precoAntigo'>De R$ 3.989,90</span>
-            <br />
-            <span className='precoProduto'>R$ 1.989,90</span>
-            <br />
-            <span className='infoProduto'>Fogões de altíssima qualidade em oferta!! Só hoje!!</span>
-            <br />
-            <button className='botaoCarrinho' onClick={() => addCarrinho("Fogão Consul 5 Bocas CFS5VAE", 1989.9)}>Adicionar ao Carrinho</button>
-          </div>
-        </div>
-      </div>
+      ))};
 
       <div className="Rodape">
         <Link to="/Sobre" className='Sobre'>Sobre a nossa empresa</Link>
@@ -170,5 +74,4 @@ function Home() {
     </div>
   );
 }
-
 export default Home;
